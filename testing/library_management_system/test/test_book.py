6dae4c28c699e22b9e3bdaa1af8285c3b4c4c430
SCCSMARTCODE <sccsmart247@gmail.com>
2025-01-24 12:16:05 +0100

    added setup function to avoid redundant write-up

--- testing/library_management_system/test/test_book.py
+++ testing/library_management_system/test/test_book.py
@@ -10,7 +10,24 @@ from testing.library_management_system.app.user import UserObj
 class BookFunctionalityTest(TestCase):
 
     def setUp(self):
-        pass
+        self.books = [
+            {
+                "title": "title",
+                "author": "author",
+                "genre": "genre",
+                "available_copies": 10,
+                "borrowed_copies": 0,
+                "transactions": []
+            },
+            {
+                "title": "title1",
+                "author": "author1",
+                "genre": "genre",
+                "available_copies": 20,
+                "borrowed_copies": 0,
+                "transactions": []
+            }
+        ]
 
     def test_load_book_with_db_not_existing(self):
         if os.path.exists(BOOKS_DB):
@@ -19,102 +36,38 @@ class BookFunctionalityTest(TestCase):
         self.assertEqual(result, [])
 
     def test_load_books_and_save_books_with_db_existing(self):
-        books = [
-            {
-                "title": "title",
-                "author": "author",
-                "genre": "genre",
-                "available_copies": 10,
-                "borrowed_copies": 0,
-                "transactions": []
-            }
-        ]
-        save_books(books)
+        save_books(self.books)
         result = load_books()
-        self.assertEqual(result, books)
+        self.assertEqual(result, self.books)
         os.remove(BOOKS_DB)
 
     def test_add_book(self):
-        books = [
-            {
-                "title": "title",
-                "author": "author",
-                "genre": "genre",
-                "available_copies": 10,
-                "borrowed_copies": 0,
-                "transactions": []
-            },
-            {
-                "title": "title1",
-                "author": "author1",
-                "genre": "genre",
-                "available_copies": 20,
-                "borrowed_copies": 0,
-                "transactions": []
-            }
-        ]
-        save_books([books[0]])
+
+        save_books([self.books[0]])
         with mock.patch('builtins.input') as patch_input:
             patch_input.side_effect = [
-                                        books[1]['title'],
-                                        books[1]['author'],
-                                        books[1]['genre'],
-                                        books[1]['available_copies']
+                                        self.books[1]['title'],
+                                        self.books[1]['author'],
+                                        self.books[1]['genre'],
+                                        self.books[1]['available_copies']
                                     ]
             add_book()
         result = load_books()
 
-        self.assertEqual(result, books)
+        self.assertEqual(result, self.books)
         os.remove(BOOKS_DB)
 
 
     def test_remove_book(self):
-        books = [
-            {
-                "title": "title",
-                "author": "author",
-                "genre": "genre",
-                "available_copies": 10,
-                "borrowed_copies": 0,
-                "transactions": []
-            },
-            {
-                "title": "title1",
-                "author": "author1",
-                "genre": "genre",
-                "available_copies": 20,
-                "borrowed_copies": 0,
-                "transactions": []
-            }
-        ]
-        save_books(books)
+        save_books(self.books)
         with mock.patch('builtins.input') as patch_input:
-            patch_input.return_value = books[1]['title']
+            patch_input.return_value = self.books[1]['title']
             remove_book()
         result = load_books()
-        self.assertEqual(result, [books[0]])
+        self.assertEqual(result, [self.books[0]])
         os.remove(BOOKS_DB)
 
     def test_update_book(self):
-        books = [
-            {
-                "title": "title",
-                "author": "author",
-                "genre": "genre",
-                "available_copies": 10,
-                "borrowed_copies": 0,
-                "transactions": []
-            },
-            {
-                "title": "title1",
-                "author": "author1",
-                "genre": "genre",
-                "available_copies": 20,
-                "borrowed_copies": 0,
-                "transactions": []
-            }
-        ]
-
         book1_updated_version = {
                 "title": "title1",
                 "author": "author1_updated",
@@ -123,7 +76,7 @@ class BookFunctionalityTest(TestCase):
                 "borrowed_copies": 0,
                 "transactions": []
             }
-        save_books(books)
+        save_books(self.books)
         with mock.patch('builtins.input') as patch_input:
             patch_input.side_effect = [
                                         book1_updated_version['title'],
@@ -137,28 +90,10 @@ class BookFunctionalityTest(TestCase):
         os.remove(BOOKS_DB)
 
     def test_borrow_book(self):
-        books = [
-            {
-                "title": "title",
-                "author": "author",
-                "genre": "genre",
-                "available_copies": 10,
-                "borrowed_copies": 0,
-                "transactions": []
-            },
-            {
-                "title": "title1",
-                "author": "author1",
-                "genre": "genre",
-                "available_copies": 20,
-                "borrowed_copies": 0,
-                "transactions": []
-            }
-        ]
-        save_books(books)
+        save_books(self.books)
         with mock.patch('builtins.input') as patch_input:
             user = UserObj(username="test", email="test@gmail.com", mode="client")
-            patch_input.return_value = books[0]['title']
+            patch_input.return_value = self.books[0]['title']
             patch_date_return_value = datetime.now()
             with mock.patch('testing.library_management_system.app.book.datetime') as patch_date:
                 patch_date.now.return_value = patch_date_return_value
@@ -166,33 +101,15 @@ class BookFunctionalityTest(TestCase):
         result = load_books()
         self.assertTrue(result[0]['transactions'])
         self.assertEqual(result[0]['transactions'][0].get("email"), user.email)
-        self.assertEqual(result[0]['available_copies'], books[0]['available_copies'] - 1)
+        self.assertEqual(result[0]['available_copies'], self.books[0]['available_copies'] - 1)
         self.assertEqual(result[0]['borrowed_copies'], 1)
         os.remove(BOOKS_DB)
 
     def test_return_book(self):
-        books = [
-            {
-                "title": "title",
-                "author": "author",
-                "genre": "genre",
-                "available_copies": 10,
-                "borrowed_copies": 0,
-                "transactions": []
-            },
-            {
-                "title": "title1",
-                "author": "author1",
-                "genre": "genre",
-                "available_copies": 20,
-                "borrowed_copies": 0,
-                "transactions": []
-            }
-        ]
-        save_books(books)
+        save_books(self.books)
         with mock.patch('builtins.input') as patch_input:
             user = UserObj(username="test", email="test@gmail.com", mode="client")
-            patch_input.return_value = books[0]['title']
+            patch_input.return_value = self.books[0]['title']
             borrow_book(user)
             return_book(user)
 
@@ -200,7 +117,7 @@ class BookFunctionalityTest(TestCase):
             self.assertTrue(result[0]['transactions'])
             self.assertTrue(result[0]['transactions'][0]["returned"])
             self.assertEqual(result[0]['transactions'][0].get("email"), user.email)
-            self.assertEqual(result[0]['available_copies'], books[0]['available_copies'])
+            self.assertEqual(result[0]['available_copies'], self.books[0]['available_copies'])
             self.assertEqual(result[0]['borrowed_copies'], 0)
             os.remove(BOOKS_DB)
 
